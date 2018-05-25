43adb560e630b5652e75a52b197785d88b124a0c
Craig Edwards <craigroe7@gmail.com>
2018-05-25 16:04:21 -0400

    fix server.js

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const path = require('path');
-// const device = require('express-device');
-
-// const port = process.env.PORT || 3000;
-// const app = express();
-
-// const { redirectToHTTPS } = require('express-http-to-https');
-
-// app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
-// app.use(device.capture());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
-// // app.use((req, res, next) => {
-// //   // Website you wish to allow to connect
-// //   res.setHeader('Access-Control-Allow-Origin', '*')
-// //   // Request methods you wish to allow
-// //   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-// //   // Request headers you wish to allow
-// //   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
-// //   // Set to true if you need the website to include cookies in the requests sent
-// //   // to the API (e.g. in case you use sessions)
-// //   res.setHeader('Access-Control-Allow-Credentials', true)
-// //   // Pass to next layer of middleware
-// //   next()
-// // })
-
-// app.get('/', (req, res) => {
-//   app.use(express.static(`${__dirname}/dist/pwa-ios/`));
-//   app.use(express.static(`${__dirname}./`));
-//   app.use(express.static(`${__dirname}/`));
-//   res.sendFile(path.join(`${__dirname}/dist/pwa-ios/index.html`));
-// });
-
-// app.listen(port, () => {
-//   console.log(`Started on port ${port}`);
-// });
-
-// module.exports = { app };
 const express = require('express');
-const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
+const path = require('path');
+const device = require('express-device');
 
-const port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(serveStatic('/dist'));
-app.listen(port);
+const { redirectToHTTPS } = require('express-http-to-https');
+
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
+app.use(device.capture());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use((req, res, next) => {
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   // Pass to next layer of middleware
+//   next()
+// })
+
+app.get('/', (req, res) => {
+  app.use(express.static(`${__dirname}/dist/pwa-ios/`));
+  app.use(express.static(`${__dirname}./`));
+  app.use(express.static(`${__dirname}/`));
+  res.sendFile(path.join(`${__dirname}/dist/pwa-ios/index.html`));
+});
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
+
+module.exports = { app };
+// const express = require('express');
+// const serveStatic = require('serve-static');
+
+// const port = process.env.PORT || 5000;
+
+// const app = express();
+
+// app.use(serveStatic('/dist'));
+// app.listen(port);
